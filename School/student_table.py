fd8bcd3d7e74d6ed141febdb76869347f05660bd
Vincent <vincenttayros078@gmail.com>
2026-05-20 17:42:11 +0800

    aded student table functions

--- School/student_table.py
+++ School/student_table.py
@@ -18,3 +18,83 @@ def student_table(conn):
         print(f"Unexpected error on student table: {e}")
         raise
 
+def add_student(conn, name):
+    try:
+       c = conn.cursor()
+       c.execute('INSERT INTO students (name) VALUES (?)',(name,))
+       conn.commit()
+       return c.lastrowid
+
+    except sqlite3.Error:
+        print("Adding student error")
+        return None
+    except Exception:
+        print("Unexpected adding in student table")
+        return None
+
+def view_all_student(conn):
+    try:
+        c = conn.cursor()
+        c.execute('SELECT id,name FROM students ')
+        return c.fetchall()
+    
+    except sqlite3.Error:
+        print("View method error ")
+        return []
+    except Exception:
+        print("Unexpected error in view method in student table")
+        return []
+
+def update_student(conn,id,name):
+    try:
+        c = conn.cursor()
+        c.execute('SELECT id,name FROM students WHERE id = ?',(id,))
+        if not c.fetchone():
+            print('Student does not exists')
+            return 0
+
+        c.execute('UPDATE students SET name = ? WHERE id = ?',(name,id))
+        conn.commit()
+        return c.rowcount
+
+    except sqlite3.Error:
+        print("Updating query error")
+        return 0
+    except Exception:
+        print("Unexpected updating error")
+        return 0
+
+def delete_student(conn,id):
+    try:
+        c = conn.cursor()
+        c.execute('SELECT id,name FROM students WHERE id = ? ',(id,))
+        if not c.fetchone():
+            print('Student does not exists')
+            return 0
+        c.execute('DELETE FROM students WHERE id = ?',(id,))
+        conn.commit()
+        return c.rowcount
+    except sqlite3.Error:
+        print('Delete query error')
+        return 0
+    except Exception:
+        print('Unexpected error on delete table')
+        return 0
+
+def  view_student_by_id(conn,id):
+    try:
+        c = conn.cursor()
+        c.execute('SELECT id,name FROM students WHERE id = ?', (id,))
+        student = c.fetchone()
+        if not student:
+            print('Student does not exists')
+            return None
+        
+        return student
+
+    except sqlite3.Error:
+        print("Error viewing id query")
+        return None
+    except Exception:
+        print("Unexpecetd error in viewing by id")
+        return None
